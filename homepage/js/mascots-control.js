b668f4a6aa525a6776bcaf0ffc28c04ee86c689d
qrenz <renzq@student.morainevalley.edu>
2017-09-17 17:58:22 -0500

    initial commit

---
+++ homepage/js/mascots-control.js
@@ -0,0 +1,34 @@
+/*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
+function controlMascot(mascot, mascotMinWidth) {
+    $(window).resize(function(event) {
+        if ( $(window).width() <= mascotMinWidth && $("main").hasClass("mascot") ) {
+            removeMascot();
+        } else if( $(window).width() > mascotMinWidth && ! $("main").hasClass("mascot") ) {
+            setMascot(mascot);
+        }
+    });
+}
+
+function setMascot(mascot) {
+    $('main').addClass("mascot");
+    $('main').css("background-image", "url(" + mascot + ")");
+    $('main').removeClass("plain");
+}
+
+function removeMascot() {
+    $('main').removeClass("mascot");
+    $('main').css("background-image", "");
+    $('main').addClass("plain");
+}
+
+$(document).ready(function(event) {
+    var mascotEnable    = true;
+
+    var mascot          = mascotPath + mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotMinWidth  = 750;
+
+    if ( mascotEnable ) {
+        setMascot(mascot);
+        controlMascot(mascot, mascotMinWidth);
+    } else { removeMascot(); }
+});
